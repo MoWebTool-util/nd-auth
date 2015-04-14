661d16e11608e265a3a6817416951de01a4f54bf
crossjs <liwenfu@crossjs.com>
2015-04-14 13:59:56 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# nd-auth
+
+[![spm version](http://spm.crossjs.com/badge/nd-auth)](http://spm.crossjs.com/package/nd-auth)
+
+> UC Authorization
+
+## 安装
+
+```bash
+$ spm install nd-auth --save
+```
+
+## 使用
+
+```js
+var auth = require('nd-auth');
+// use auth
+```
---
+++ index.js
@@ -0,0 +1,120 @@
+/**
+ * @module: nd-auth
+ * @author: crossjs <liwenfu@crossjs.com> - 2015-04-14 13:54:53
+ */
+
+'use strict';
+
+var Storage = require('nd-storage');
+var Sha = require('nd-sha');
+var datetime = require('nd-datetime');
+
+function nonce() {
+  function rnd(min, max) {
+    var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    var range = max ? max - min : min,
+      str = '',
+      i,
+      length = arr.length - 1;
+
+    for (i = 0; i < range; i++) {
+      str += arr[Math.round(Math.random() * length)];
+    }
+
+    return str;
+  }
+
+  return new Date().getTime() + ':' + rnd(8);
+}
+
+// {
+//   "access_token": "7B5A07FC9B656ED2F640991E1D69557CD25DA30B2C44B497F5E5AE774B2B306C18B590A71B3F069F",
+//   "expires_at": "2015-04-20T21:58:32.830+0800",
+//   "mac_algorithm": "hmac-sha-256",
+//   "mac_key": "NY3YSdOTcQ",
+//   "refresh_token": "7B5A07FC9B656ED2F65ED04CA451FDB75CE0D8C523CB99CC2C59347628E936ABDCBB9DA2856A945D",
+//   "server_time": "2015-04-13T21:58:32.839+0800",
+//   "user_id": 2107039396
+// }
+
+module.exports = {
+
+  tokenKey: 'UC_TOKENS',
+
+  tokens: null,
+
+  isAuthed: function() {
+    return !!this.getTokens();
+  },
+
+  getTokens: function(key) {
+    var tokens = this.tokens;
+
+    if (!tokens) {
+      // 本地存储
+      tokens = Storage.get(this.tokenKey);
+
+      if (tokens) {
+        tokens = JSON.parse(tokens);
+      }
+    }
+
+    if (tokens) {
+      // 失效判断
+      if (datetime(tokens['expires_at']).toNumber() <= datetime().toNumber()) {
+        tokens = null;
+        this.setTokens(tokens);
+      }
+    }
+
+    if (tokens) {
+      this.tokens = tokens;
+    }
+
+    if (key && tokens) {
+      return tokens[key];
+    }
+
+    return tokens;
+  },
+
+  /**
+   * 设置或清除 tokens
+   * @param {object} tokens token值
+   */
+  setTokens: function(tokens) {
+    this.tokens = tokens;
+
+    if (tokens === null) {
+      Storage.remove(this.tokenKey);
+    } else {
+      Storage.set(this.tokenKey, JSON.stringify(tokens));
+    }
+  },
+
+  _getAccessToken: function() {
+    return this.getTokens('access_token');
+  },
+
+  _getMacContent: function(method, url, host) {
+    return [this.nonce, method, url, host, ''].join('\n');
+  },
+
+  _getMac: function(method, url, host) {
+    return new Sha(this._getMacContent(method, url, host), 'TEXT')
+          .getHMAC(this.getTokens('mac_key'), 'TEXT', 'SHA-256', 'B64');
+  },
+
+  _getNonce: function() {
+    this.nonce = nonce();
+    return this.nonce;
+  },
+
+  getAuthentization: function(method, url, host) {
+    return ['MAC id="' + this._getAccessToken() + '"',
+            'nonce="' + this._getNonce() + '"',
+            'mac="' + this._getMac(method, url, host) + '"'].join(',');
+  }
+
+};
